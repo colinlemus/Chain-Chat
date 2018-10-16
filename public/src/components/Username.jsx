de3f9f7eb262415aa1263adcd9fb2226a1c03541
imalam64 <imalam64@gmail.com>
2018-10-15 21:11:07 -0700

    created boiler plate for comps

---
+++ public/src/components/Username.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Username extends React.Component{
+constructor(props) {
+  super(props);
+}
+render() {
+  return (
+    <div></div>
+  );
+}
+}
+
+export default Username;
