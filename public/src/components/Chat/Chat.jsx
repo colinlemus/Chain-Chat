ac6ccc860309f71a98f6386430db4ea98837dc43
imalam64 <imalam64@gmail.com>
2018-11-05 20:42:46 -0800

    worked on CSS for front-end

--- public/src/components/Chat/Chat.jsx
+++ public/src/components/Chat/Chat.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import ChatBox from './ChatBox';
 import ChatInput from './ChatInput';
 import openSocket from 'socket.io-client';
+import Grid from '@material-ui/core/Grid'
 
 const socket = openSocket('http://localhost:8081');
 
 class Chat extends React.Component {
     render() {
         return (
-            <div>
-                <div className="row">
+            <Grid container spacing={24}>
+                <Grid item xs={12}>
                         <ChatBox socket={socket} />
-                </div>
+                </Grid>
                 <ChatInput socket={socket} />
-            </div>
+            </Grid>
         );
     }
 }
