44cfafcfcc2f4e0f192313fd61293bb1e7db9ee1
imalam64 <imalam64@gmail.com>
2018-11-06 20:41:37 -0800

    added logo to confirmation and logout button

--- public/src/components/auth/Confirmation.jsx
+++ public/src/components/auth/Confirmation.jsx
@@ -24,34 +24,50 @@ export default class Confirmation extends Component {
     render() {
         if (this.state.verified) {
             return (
-                <div className="row">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 mb-3">
+                            <img src='./ChainChatLogo.png' className="rounded mx-auto d-block" alt="Responsive" width='722px' height='282px' />
+                        </div>
+                    </div>
+                    <div className="row">
                     <div className="col-sm-6 m-auto">
                         <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Verification Status</h5>
+                        <div class="card-header">
+                            Verification Status
+                        </div>
+                        <div className="card-body m-auto">
                             <p className="card-text">Verified!</p>
                             <a href="/" className="btn btn-primary">Login</a>
                         </div>
                         </div>
+                    </div>
                     </div>
                 </div>
             );
         }
 
         return (
-            <div className='container'>
-            <div className="row ">
+            <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 mb-3">
+                            <img src='./ChainChatLogo.png' className="rounded mx-auto d-block" alt="Responsive" width='722px' height='282px' />
+                        </div>
+                    </div>
+                    <div className="row">
                     <div className="col-sm-6 m-auto">
                         <div className="card">
-                        <div className="card-body">
-                            <h5 className="card-title">Verification Status</h5>
-                            <p className="card-text">Not verified!</p>
+                        <div class="card-header">
+                            Verification Status
+                        </div>
+                        <div className="card-body m-auto">
+                            <p className="card-text"> Not Verified!</p>
                             <a href="/" className="btn btn-primary">Login</a>
                         </div>
                         </div>
                     </div>
+                    </div>
                 </div>
-            </div>
         );
     }
 }
